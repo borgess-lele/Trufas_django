e263ccd33182f432ac127ed93f6a644e572c3c28
Leticia <leticiaborgess.oficial@gmail.com>
2023-09-18 09:29:16 -0300

    imagem

--- trufas/models/combo.py
+++ trufas/models/combo.py
@@ -1,7 +1,19 @@
 from django.db import models
 
+from uploader.models import Image
+
 class Combo (models.Model):
     descricao = models.CharField(max_length=100)
+    capa = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None,
+    )
+
+
 
     def __str__(self):
         return self.descricao
--- trufas/serializers/combo.py
+++ trufas/serializers/combo.py
@@ -2,7 +2,25 @@ from rest_framework.serializers import ModelSerializer
 
 from trufas.models import Combo
 
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
+
+
 class ComboSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source="capa",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Combo
         fields = "__all__"
+
+class ComboDetailSerializer(ModelSerializer):
+        capa = ImageSerializer(required=False)
